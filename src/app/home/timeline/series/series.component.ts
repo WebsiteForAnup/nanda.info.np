6a0f59184c843221f6cd38553e5b17414199f5fe
Simle Infotech <robot@sip.com.np>
2022-09-27 19:35:35 +0545

    timeline as css

--- src/app/home/timeline/series/series.component.ts
+++ src/app/home/timeline/series/series.component.ts
@@ -1,14 +1,4 @@
 import { Component, OnInit } from '@angular/core';
-import { ChangeDetectionStrategy } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
-
-class Account {
-  constructor(readonly name: string, readonly balance: number) {}
-
-  toString(): string {
-    return `${this.name} (${this.balance})`;
-  }
-}
 
 @Component({
   selector: 'app-series',
@@ -16,17 +6,6 @@ class Account {
   styleUrls: ['./series.component.scss'],
 })
 export class SeriesComponent implements OnInit {
-  readonly accounts = [new Account(`Rubles`, 500), new Account(`Dollar`, 237), new Account(`Euro`, 100)];
-
-  svgIcons = {
-    rubles: import(`assets/rubles.svg`),
-  };
-
-  testForm = new FormGroup({
-    name: new FormControl(``),
-    accounts: new FormControl(this.accounts[0]),
-  });
-
   constructor() {}
 
   ngOnInit(): void {}
